cc1057ceb8c237b2d015d651bb9d57a692bc490a
JuanEduardo <juaneduardohv@gmail.com>
2015-08-08 12:44:45 -0300

    executives and minor changes in Boards

--- svs_mapper/spiders/executives.py
+++ svs_mapper/spiders/executives.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.selector import Selector
+from scrapy import Selector
 from svs_mapper.items import Directory
 from scrapy.http import Request
 from datetime import date
 
 
-class SvsSpider(scrapy.Spider):
-    name = "svs"
+class ExecutivesSpider(scrapy.Spider):
+    name = "executives"
     allowed_domains = ["svs.cl"]
     base_url = 'http://www.svs.cl/institucional/mercados/'
-    start_urls = [
-        'http://www.svs.cl/institucional/mercados/listado_actual_directorios.php?mercado=V',
-    ]
-    today = date.today()
+    start_urls = (
+        'http://www.svs.cl/institucional/mercados/listado_actual_ejecutivos.php?mercado=V',
+    )
 
     def parse(self, response):
-
         hxs = Selector(response)
 
         company_list = hxs.xpath('//tr[td]')
@@ -24,8 +22,8 @@ class SvsSpider(scrapy.Spider):
         for item in company_list:
             data = Directory()
             data['company_ICN'] = ''.join(item.xpath('td[1]/a/text()').extract()).strip()
-            data['id'] = 'Board_' + data['company_ICN']
-            data['type'] = 'Board'
+            data['id'] = 'Executives_' + data['company_ICN']
+            data['type'] = 'Executives'
             data['company_name'] = ''.join(item.xpath('td[2]/a/text()').extract()).strip()
             link = ''.join(item.xpath('td[2]/a/@href').extract()).strip()
             request = Request(self.base_url + link,
@@ -45,6 +43,7 @@ class SvsSpider(scrapy.Spider):
             data['person_ICN'] = ''.join(item.xpath('td[1]/text()').extract()).strip()
             data['person_name'] = ''.join(item.xpath('td[2]/text()').extract()).strip()
             data['person_jobtitle'] = ''.join(item.xpath('td[3]/text()').extract()).strip()
-            data['person_datejob'] = ''.join(item.xpath('td[4]/text()').extract()).strip()
+            data['person_jobtitle_desc'] = ''.join(item.xpath('td[4]/text()').extract()).strip()
+            data['person_datejob'] = ''.join(item.xpath('td[5]/text()').extract()).strip()
             data['scanner_date'] = self.today
             yield data
